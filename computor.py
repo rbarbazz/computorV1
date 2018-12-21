2bc0db95dfb674f55310dca206b3b94a1595cbd4
rbarbazz <rbarbazz@student.42.fr>
2018-12-21 20:25:20 +0100

    parsing ok

--- computor.py
+++ computor.py
@@ -7,9 +7,24 @@ def get_input():
 	parser.add_argument('equation', help='Polynomial equation to be solved')
 	return parser.parse_args().equation
 
-def format_input(raw_input):
+def rm_wspaces(raw_input):
 	return re.sub(r'\s+', '', raw_input)
 
+def get_terms(str):
+	matchs = re.search(r'([+-]?\d+(\.)?(?(2)\d+)\*X\^0)|([+-]?\d+(\.)?(?(4)\d+)\*X\^1)|([+-]?\d+(\.)?(?(6)\d+)\*X\^2)', str)
+	print(matchs.group(3))
+
+class Equation:
+	def __init__(self, raw_input):
+		input = rm_wspaces(raw_input)
+		self.left = input.split('=')[0]
+		self.right = input.split('=')[1]
+
+
+
 raw_input = get_input()
-equation = format_input(raw_input)
-print(equation)
+equals_count = raw_input.count('=')
+if equals_count != 1:
+	sys.exit('Wrong format: found {} equals sign'.format(equals_count))
+equation = Equation(raw_input)
+get_terms(equation.left)
