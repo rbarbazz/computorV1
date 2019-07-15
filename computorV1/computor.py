bab2b195bc58fd59021c67ba754b532f6775dc46
rbarbazz <raphael.barbazza@gmail.com>
2019-07-15 16:48:16 +0200

    code refactor

--- computorV1/computor.py
+++ computorV1/computor.py
@@ -7,43 +7,45 @@ from equation import Equation
 # When the program is called from CLI
 def get_input():
     parser = argparse.ArgumentParser(
-        description='Solve a polynomial equation up to degree 2')
+        description='Solve a polynomial equation up to degree 2'
+    )
     parser.add_argument('equation', help='Format: \"n * X^[0-2]\"')
     return parser.parse_args().equation
-
-
-def error_handler(err_code, equals_count=0):
-    err = [
-        'Wrong format: found {} equals sign'.format(equals_count),
-        'Wrong format: check that you formatted the equation correctly',
-        'Reduced form: 0 * X^0 = 0 * X^0\nPolynomial degree: 0\nAll real numbers are solution',
-        'The polynomial degree is stricly greater than 2, I can\'t solve.',
-        'Equation is invalid'
-    ]
-    return err[err_code]
 
 
 def solve_equ(input):
     solution = ''
     input = re.sub(r'\s+', '', input)
+
+    # Check the count of equal sign
     equals_count = input.count('=')
-
     if equals_count != 1:
-        return solution + error_handler(0, equals_count)
+        return 'Wrong format: found {} equal signs'.format(equals_count)
 
+    # Simplify both terms of the equation
     equation = Equation(input)
     if not equation.check_terms():
-        return solution + error_handler(1)
-    if equation.get_reduced_form() == 'Reduced form: = 0\n':
-        return solution + error_handler(2)
+        return 'Wrong format: check that you properly formatted the equation'
+    if not equation.get_reduced_form():
+        return (
+            'Reduced form: 0 * X^0 = 0\n' +
+            'Polynomial degree: 0\n' +
+            'All real numbers are solution'
+        )
 
-    solution += equation.get_reduced_form()
-    solution += ('Polynomial degree: ' + str(equation.get_degree()) + '\n')
+    solution += 'Reduced form: {}= 0\n'.format(equation.reduced_form)
+    solution += 'Polynomial degree: {}\n'.format(equation.get_degree())
     if equation.degree > 2:
-        return solution + error_handler(3)
+        return (
+            solution +
+            'The polynomial degree is stricly greater than 2' +
+            ', sorry I can\'t solve it.'
+        )
     elif equation.degree == 0:
-        return solution + error_handler(4)
-    return solution + equation.solve()
+        return solution + 'Equation is invalid'
+
+    solution += equation.solve()
+    return re.sub(r'\.0 ', ' ', solution)
 
 
 if __name__ == '__main__':
@@ -51,6 +53,11 @@ if __name__ == '__main__':
     print(solve_equ(input))
 
 # Bonus :
-# - floats
+# - natural form inputs: a * X^n, a * X, X^n, a
 # - formatting error handling : wrong degree, wrong format
 # - webapp
+
+
+# Todo:
+# - recode sqrt
+# - complex solutions
--- webapp/solver/computorV1/equation.py
+++ webapp/solver/computorV1/equation.py
@@ -1,58 +1,75 @@
 import re
+import math
 
 
 class Equation:
     def __init__(self, input):
-        self.left = input.split('=')[0]
-        self.right = input.split('=')[1]
+        equation_parts = input.split('=')
+        self.left = equation_parts[0]
+        self.right = equation_parts[1]
         self.terms = {term: 0 for term in range(0, 3)}
         self.degree = 0
+        self.reduced_form = ''
 
-    # Check if both sides are valid
+    # Check if both terms are valid
     def check_terms(self):
         return (
-            self.get_term(self.left, False) and
+            self.get_term(self.left) and
             self.get_term(self.right, True)
         )
 
-    # Match a * X^N, add and sub recurring values and makes a dict {a : N}
-    def get_term(self, equation_part, isRight):
+    # Get reduced form as a dict {exp: N}
+    def get_term(self, equation_part, isRight=False):
         terms_len = 0
-        matches = re.finditer(
-            r'(?P<val>[+-]?\d+(\.)?(?(2)\d+))\*X\^(?P<pow>\d+)', equation_part
-        )
+        reg = re.compile('(?P<coef>[-+]?[0-9]*\.[0-9]+|[-+]?[0-9]+)(?P<pow>\*X(\^(?P<exp>\d+))?)?|(?P<xpow>[-+]?X\^(?P<xexp>\d+))')
+        matches = re.finditer(reg, equation_part)
 
         for match in matches:
-            i = int(match.group('pow'))
-            if i not in self.terms.keys():
-                self.terms.update({i: 0})
-            if isRight:
-                self.terms[i] -= float(match.group('val'))
+            # Check if the match is a * X^n or a number
+            # Format: a * X^n or a * X
+            if match.group('pow') is not None:
+                coef = float(match.group('coef'))
+                exp = 1
+                if match.group('exp') is not None:
+                    exp = int(match.group('exp'))
+            # Format: X^n
+            elif match.group('xpow') is not None:
+                coef = 1
+                exp = int(match.group('xexp'))
+            # Format: a
             else:
-                self.terms[i] += float(match.group('val'))
+                coef = float(match.group('coef'))
+                exp = 0
+
+            # Add value found to the reduced form
+            if exp not in self.terms.keys():
+                self.terms[exp] = 0
+            if isRight:
+                self.terms[exp] -= float(coef)
+            else:
+                self.terms[exp] += float(coef)
+
             terms_len += len(match.group(0))
+
         return terms_len == len(equation_part) and len(equation_part) != 0
 
     def get_reduced_form(self):
         reduced_form = ''
+
         for term, value in self.terms.items():
             if value != 0:
-                reduced_form += '{}{}{} * X^{} '.format(
-                    ["", "+ "][value > 0],
-                    ["", "- "][value < 0],
+                sign = '+' if value > 0 else '-'
+                reduced_form += '{} {} * X^{} '.format(
+                    sign,
                     abs(value),
-                    str(term)
+                    term
                 )
-        reduced_form = re.sub(r'^\+ ', '', reduced_form)
-        return (
-            'Reduced form: ' +
-            re.sub(r'\.0 ', ' ', reduced_form) +
-            '= 0\n'
-        )
+        self.reduced_form = re.sub(r'^\+ ', '', reduced_form)
+        return self.reduced_form != ''
 
     def get_degree(self):
         for term, value in self.terms.items():
-            if value != 0:
+            if value != 0 and term > self.degree:
                 self.degree = term
         return self.degree
 
@@ -62,22 +79,34 @@ class Equation:
         c = self.terms[0]
 
         if self.degree == 1:
-            solution = 'The solution is:\n' + str(-c / b)
+            return 'The solution is:\n{} '.format(-c / b)
         else:
             self.delta = b ** 2 - (4 * a * c)
-            solution = 'Discriminant: ' + str(self.delta)
-            if self.delta < 0:
-                solution += '\nThe discriminant is stricly negative, there are two complex solution:\n'
-                first_part = str(-b / (2 * a))
-                sec_part = str(((-self.delta) ** 0.5) / (2 * a))
-                solution += first_part + '- ' + sec_part + 'i\n'
-                solution += first_part + '+ ' + sec_part + 'i'
-            elif self.delta == 0:
-                solution += '\nThe discriminant is null, there is one solution:\n'
-                solution += str(-b / (2 * a))
-            else:
-                solution += '\nThe discriminant is stricly positive, there are two solutions:\n'
-                solution += str((-b - self.delta ** 0.5) / (2 * a)) + '\n'
-                solution += str((-b + self.delta ** 0.5) / (2 * a))
+            solution = 'Discriminant: {} \n'.format(self.delta)
 
-        return re.sub(r'\.0', ' ', solution)
+            if self.delta < 0:
+                solution += (
+                    'The discriminant is stricly negative, ' +
+                    'there are two complex solution:\n'
+                )
+                # first_part = str(-b / (2 * a))
+                # sec_part = str(((-math.sqrt(self.delta))) / (2 * a))
+                # solution += first_part + ' + i * ' + sec_part + '\n'
+                # solution += first_part + ' - i * ' + sec_part
+            elif self.delta == 0:
+                solution += (
+                    'The discriminant is null, ' +
+                    'there is one solution:\n'
+                )
+                solution += '{} '.format(-b / (2 * a))
+            else:
+                solution += (
+                    'The discriminant is stricly positive, ' +
+                    'there are two solutions:\n'
+                )
+                solution += '{} \n{} '.format(
+                    (-b - math.sqrt(self.delta)) / (2 * a),
+                    (-b + math.sqrt(self.delta)) / (2 * a)
+                )
+
+        return solution
